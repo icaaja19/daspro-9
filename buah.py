1c6104b014eea911741576cf2dc7a0b67484d6fc
icaaja19 <rinjaniraisha8@gmail.com>
2025-04-30 20:59:30 +0700

    Add files via upload

---
+++ buah.py
@@ -0,0 +1,9 @@
+list_buah = ['mangga', 'apel', 'stoberi', 'jeruk', 'duren']
+print(list_buah)
+
+print("Buah pada indeks ke-2 adalah:",list_buah[2])
+buah_baru =input("Masukan nama buah baru yang akan ditambahkan :")
+list_buah.append(buah_baru)
+
+print("daftar buah setelah ditambahkan :")
+print(list_buah)
